e059a2156b1eb24c8b0a305f535e53d988b427db
chrisonoo <chris@onoo.no>
2025-02-07 15:49:42 +0100

    feat: implement Light/Dark mode initialization and toggle functionality

--- src/components/LightDarkToggle.tsx
+++ src/components/LightDarkToggle.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import { SunIcon, MoonIcon, DesktopIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 
 type Props = {
@@ -7,12 +7,56 @@ type Props = {
 };
 
 export function LightDarkToggle({ className }: Props) {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const localStorageTheme =
+    localStorage.theme !== undefined ? localStorage.theme : false;
+  const isSystemThemeDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  let themeModeState = 0;
+
+  if (localStorageTheme === false || localStorageTheme === "dark") {
+    themeModeState = 0;
+  } else if (localStorageTheme === "light") {
+    themeModeState = 1;
+  } else {
+    themeModeState = 2;
+  }
+
+  const [themeMode, setThemeMode] = useState(themeModeState);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setIsDarkMode((prevValue) => !prevValue);
-    document.documentElement.classList.toggle("dark");
+    setThemeMode((prevValue) => {
+      const newValue = (prevValue + 1) % 3;
+      switch (newValue) {
+        case 0:
+          localStorage.theme = "dark";
+          document.documentElement.classList.add("dark");
+          break;
+        case 1:
+          localStorage.theme = "light";
+          document.documentElement.classList.remove("dark");
+          break;
+        case 2:
+          localStorage.theme = "";
+          document.documentElement.classList.toggle("dark", isSystemThemeDark);
+          break;
+      }
+      return newValue;
+    });
+  };
+
+  const getIcon = () => {
+    switch (themeMode) {
+      case 0:
+        return <MoonIcon />;
+      case 1:
+        return <SunIcon />;
+      case 2:
+        return <DesktopIcon />;
+      default:
+        return <MoonIcon />;
+    }
   };
 
   return (
@@ -23,7 +67,7 @@ export function LightDarkToggle({ className }: Props) {
       type="button"
       onClick={handleClick}
     >
-      {isDarkMode ? <MoonIcon /> : <SunIcon />}
+      {getIcon()}
     </Button>
   );
 }
