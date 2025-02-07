a732ef61a4750909e9fa0518f2323c883dea5e00
chrisonoo <chris@onoo.no>
2025-02-07 23:55:54 +0100

    feat: add LinkButton component and update index page layout

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, type ButtonProps } from "@/components/ui/button";
+
+export interface LinkProps extends ButtonProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const LinkButton: React.FC<LinkProps> = ({ href, children, ...props }) => {
+  return (
+    <Button {...props} asChild>
+      <a href={href}>{children}</a>
+    </Button>
+  );
+};
+
+export { LinkButton };
